af2f792ae86e21f5a2f51f44fca0055ef63a5e1e
Stanislav <69311715+rawkingo@users.noreply.github.com>
2020-08-26 00:54:14 +0300

    Add files via upload

---
+++ Triangleofeights.java
@@ -0,0 +1,18 @@
+/*
+Triangle of eights
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+       for (int i = 0; i < 10; i++){
+            for (int a = 0; a < i; a++){
+                System.out.print(8);
+               
+            }
+            
+                System.out.println(8);
+            
+        }
+
+    }
+}
